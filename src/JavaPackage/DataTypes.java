6b73a14d21d9a6ba2f9034e20a28587b6c66917a
Arif Mirza <rf.mirza@gmail.com>
2020-06-16 13:13:53 -0500

    this is java floders from eclipse

---
+++ src/JavaPackage/DataTypes.java
@@ -0,0 +1,33 @@
+package JavaPackage;
+
+public class DataTypes {
+
+		//This is my first Java code
+		/**
+		 * this is data types concept
+		 * we are going to cover data types in java  
+		 */
+	
+	public static void main(String[] args) {
+		
+		//data types	
+		
+		byte b = 10;
+			b = 20;
+		byte b1 =0;
+		byte b2 =-1;
+		
+		System.out.println(b);
+		System.out.println(b2+b);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
